eb3f441e6e1fcc91248a9bc08251910a00d9b2af
Ahcene Belhadi <abelhadi@c4r1p3.42nice.fr>
2024-02-27 18:10:58 +0100

    pushing to docker hub

--- srcs/files/backend/core/profiles/models.py
+++ srcs/files/backend/core/profiles/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE) 
 	bio = models.TextField(max_length=250, blank=True)
 	pseudo = models.CharField(max_length=12, blank=True)
 	rank = models.CharField(max_length=12, blank=True)
--- srcs/files/backend/core/profiles/urls.py
+++ srcs/files/backend/core/profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+	path('register/', views.register.as_view(), name='register'),
 	path('', views.get_profiles),
 	path('<int:pk>/', views.get_profile),
 	path('create/', views.create_profile),
