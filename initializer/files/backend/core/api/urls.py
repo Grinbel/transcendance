9a8dbbf627623174a4983560616eb51300dcdc3a
ahcene <abelhadi@student.42nice.fr>
2024-03-05 17:48:01 +0100

    pushing to docker hub

--- initializer/files/backend/core/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('login/', views.login_view, name='login'),
-	path('logout/', views.logout_view, name='logout'),
-	path('session/', views.session_view, name='session'),
-	path('whoami/', views.whoami_view, name='whoami'),
-]
