687993bccc71727051a1c23bb6aaf23b7c5c1246
Ahcene Belhadi <abelhadi@c2r7p6.42nice.fr>
2024-03-18 17:57:28 +0100

    pushing to docker hub

---
+++ srcs/files/frontend/core/src/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Login() {
+        return (
+            <div>
+                <h2>Login page</h2>
+            </div>
+        )
+}
+
+export default Login;
