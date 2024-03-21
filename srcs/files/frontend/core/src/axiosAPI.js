31ba9e6746367ba46ae4f0166756ad9238d92b6d
ahcene <abelhadi@student.42nice.fr>
2024-03-21 17:29:50 +0100

    pushing to docker hub

--- srcs/files/frontend/core/src/axiosAPI.js
+++ srcs/files/frontend/core/src/axiosAPI.js
@@ -9,3 +9,42 @@ export const axiosInstance = axios.create({
         'accept': 'application/json'
     }
 });
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+      const originalRequest = error.config;
+      
+      if (error.response.status === 401 && error.response.statusText === "Unauthorized") {
+          const refresh_token = localStorage.getItem('refresh_token');
+
+          return axiosInstance
+              .post('/token/refresh/', {refresh: refresh_token})
+              .then((response) => {
+
+                  localStorage.setItem('access_token', response.data.access);
+                  localStorage.setItem('refresh_token', response.data.refresh);
+
+                  axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+                  originalRequest.headers['Authorization'] = "JWT " + response.data.access;
+
+                  return axiosInstance(originalRequest);
+              })
+              .catch(err => {
+                  console.log(err)
+              });
+      }
+      return Promise.reject(error);
+  }
+);
+
+// AXIOS ERROR TYPES AND ERROR 
+// https://github.com/axios/axios?tab=readme-ov-file#handling-errors
+
+// Property 	Definition
+// message 	A quick summary of the error message and the status it failed with.
+// name 	This defines where the error originated from. For axios, it will always be an 'AxiosError'.
+// stack 	Provides the stack trace of the error.
+// config 	An axios config object with specific instance configurations defined by the user from when the request was made
+// code 	Represents an axios identified error. The table below lists out specific definitions for internal axios error.
+// status 	HTTP response status code. See here for common HTTP response status code meanings.
