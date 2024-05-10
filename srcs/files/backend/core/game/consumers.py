f52fb05bc9bdc44e21a1ef60a370d08e17fc09f9
scelios <scelios@outlook.fr>
2024-05-10 15:25:33 +0200

    indent bad

--- srcs/files/backend/core/game/consumers.py
+++ srcs/files/backend/core/game/consumers.py
@@ -5,19 +5,19 @@ from asgiref.sync import async_to_sync
 from users.helper import listUsers
 from users.models import User
 class GameConsumer(WebsocketConsumer):
-    def connect(self):
-        print('connected to game consumer')
+	def connect(self):
+		print('connected to game consumer')
 		room_name = self.scope['url_route']['kwargs']['room_name']
-        
+		
 		self.accept()
 
-    def disconnect(self, close_code):
-        pass
+	def disconnect(self, close_code):
+		pass
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        # message = text_data_json['message']
-        print('received message',text_data_json)
-        # self.send(text_data=json.dumps({
-        #     'message': message
-        # }))
+	def receive(self, text_data):
+		text_data_json = json.loads(text_data)
+		# message = text_data_json['message']
+		print('received message',text_data_json)
+		# self.send(text_data=json.dumps({
+		#     'message': message
+		# }))
